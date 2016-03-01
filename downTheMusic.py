f9a72111bfdedb40944026124982262eb42c81ff
Aaron Ouellette <aouellette2016@gmail.com>
2016-03-01 09:53:14 -0500

    major cleanup

--- downTheMusic.py
+++ downTheMusic.py
@@ -8,36 +8,49 @@ import sys
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.parse import quote_plus as qp
+import readline
 
-search = input("Enter songname / lyrics / artist ... or whatever\n> ")
-if search == "":
-    sys.exit()
+def download(link):
+    """
+    Download a video as audio from youtube given a link
+    """
+    flags = "--extract-audio --audio-format mp3 --audio-quality 0 "
+    if "youtube.com" not in link:
+        link = "http://youtube.com/" + link
+    os.system("youtube-dl " + flags + link)
 
-print("Searching ...")
+def main():
+    search = input("Enter songname / lyrics / artist ... or whatever\n> ")
+    if search == "":
+        sys.exit()
 
-# Magic happens here.
-search = qp(search)
-response = urlopen("https://www.youtube.com/results?search_query=" + search)
-html = response.read()
-soup = BeautifulSoup(html, "html.parser")
-for link in soup.find_all('a'):
-    if "/watch?v=" in link.get("href"):
-        # May change when YouTube gets updated in the future.
-        video_link = link.get("href")
-        break
+    print("Searching ...")
 
-# Print title and prompt to download
-title = title = soup.find("a", "yt-uix-tile-link").text
-print("Found: " + title)
-prompt = input("Download song (y/n)? ")
-if prompt != "y":
-    sys.exit()
+    # Magic happens here.
+    search = qp(search)
+    response = urlopen("https://www.youtube.com/results?search_query=" + search)
+    html = response.read()
+    soup = BeautifulSoup(html, "html.parser")
+    for link in soup.find_all('a'):
+        if "/watch?v=" in link.get("href"):
+            # May change when YouTube gets updated in the future.
+            video_link = link.get("href")
+            break
 
-# Links are relative on page, making them absolute.
-video_link = "http://www.youtube.com/" + video_link
-command = "youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 " + video_link
+    # Print title and prompt to download
+    title = soup.find("a", "yt-uix-tile-link").text
+    print("Found: " + title)
+    prompt = input("Download song (Y/n)? ")
+    if prompt.lower() == "n":
+        return
 
-# Download the song.
-print ("Downloading...")
-os.system(command)
-print("Done!")
+    # Download the song.
+    print ("Downloading...")
+    download(video_link)
+    print("Done!")
+    return
+
+if __name__ == "__main__":
+    while True:
+        main()
+
